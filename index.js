5e4e178facda90292f843e621ecedb2dbc47c1a5
Hisam WP <36986744+hisamwp@users.noreply.github.com>
2019-12-08 21:10:25 +0700

    init tugas4
    upload file tugas 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 (function() {
 
   glUtils.SL.init({ callback: function() { main(); } });
@@ -5,22 +6,23 @@
   var shaders = [];
   var canvas = document.getElementById("glcanvas");
   var gl = glUtils.checkWebGL(canvas);
-  
-  var scale = 0;
+
+  var scale = 2;
   var adder = 0.0026;
 
   var xAdders = 0.04;
   var yAdders = 0.03;
   var zAdders = 0.02;
-  var translate = [0.0, 0.0, 0.0];
-  var rotAdder = 0.5;
+  var translate = [0, 0, -0.5];
 
-  var theta = [0.0, 0.0, 0.0];
+  var current_middle = [0, 0, 0];
+
+  var theta = [0, 0, 0];
   var axis = 0;
   var xAxis = 0;
   var yAxis = 1;
   var zAxis = 2;
-
+  
   function initShaders() {
     var vertexShader = glUtils.getShader(gl, gl.VERTEX_SHADER, glUtils.SL.Shaders.v1.vertex),
         fragmentShader = glUtils.getShader(gl, gl.FRAGMENT_SHADER, glUtils.SL.Shaders.v1.fragment);
@@ -32,82 +34,58 @@
   }
 
   function main() {
+    var canvas = document.getElementById("glcanvas");
+    var gl = glUtils.checkWebGL(canvas);
   
     // Inisialisasi shaders dan program
     initShaders();
 
-    // gl.useProgram(shaders[0]);
-
-    // Definisi verteks dan buffer
-    /**
-     * A (  -0.5, -0.5, 0.5)
-     * B (  -0.5, 0.5,  0.5)
-     * C (  0.5,  0.5,  0.5)
-     * D (  0.5, -0.5,  0.5)
-     * E (  -0.5, -0.5, -0.5)
-     * F (  -0.5, 0.5,  -0.5)
-     * G (  0.5,  0.5,  -0.5)
-     * H (  0.5, -0.5,  -0.5)
-     */
     var cubeVertices = [
-      // x, y, z      r, g, b
-      -0.5,  0.5,   0.5,   1.0, 0.0, 0.0, //BAD BDC MERAH DEPAN
-      -0.5, -0.5,   0.5,   1.0, 0.0, 0.0,
-       0.5, -0.5,   0.5,   1.0, 0.0, 0.0,
-      -0.5, -0.5,   0.5,   1.0, 0.0, 0.0,
-       0.5, -0.5,   0.5,   1.0, 0.0, 0.0,
-       0.5,  0.5,   0.5,   1.0, 0.0, 0.0,
-      -0.5,  0.5,   0.5,   1.0, 0.0, 0.0,
-       0.5,  0.5,   0.5,   1.0, 0.0, 0.0,
+      // x, y, z            u, v         normal
 
-       0.5,  0.5,   0.5,   0.0, 1.0, 0.0, //CDH CHG HIJAU KANAN
-       0.5, -0.5,   0.5,   0.0, 1.0, 0.0,
-       0.5, -0.5,  -0.5,   0.0, 1.0, 0.0,
-       0.5, -0.5,   0.5,   0.0, 1.0, 0.0,
-       0.5, -0.5,  -0.5,   0.0, 1.0, 0.0,
-       0.5,  0.5,  -0.5,   0.0, 1.0, 0.0,
-       0.5,  0.5,   0.5,   0.0, 1.0, 0.0,
-       0.5,  0.5,  -0.5,   0.0, 1.0, 0.0,
+      //-0.5,  0.5,  0.5,     0.0, 1.0,  0.0, 0.0, 1.0, // depan, merah, BAD BDC
+      //-0.5, -0.5,  0.5,     0.0, 0.0,  0.0, 0.0, 1.0, 
+      // 0.5, -0.5,  0.5,     1.0, 0.0,  0.0, 0.0, 1.0, 
+      //-0.5,  0.5,  0.5,     0.0, 1.0,  0.0, 0.0, 1.0, 
+      // 0.5, -0.5,  0.5,     1.0, 0.0,  0.0, 0.0, 1.0, 
+      // 0.5,  0.5,  0.5,     1.0, 1.0,  0.0, 0.0, 1.0, 
 
-       0.5, -0.5,   0.5,   0.0, 0.0, 1.0, //DAE DEH BIRU BAWAH
-      -0.5, -0.5,   0.5,   0.0, 0.0, 1.0,
-      -0.5, -0.5,  -0.5,   0.0, 0.0, 1.0,
-      -0.5, -0.5,   0.5,   0.0, 0.0, 1.0,
-      -0.5, -0.5,  -0.5,   0.0, 0.0, 1.0,
-       0.5, -0.5,  -0.5,   0.0, 0.0, 1.0,
-       0.5, -0.5,   0.5,   0.0, 0.0, 1.0,
-       0.5, -0.5,  -0.5,   0.0, 0.0, 1.0,
+       0.5,  0.5,  0.5,     0.0, 1.0,  1.0, 0.0, 0.0, // kanan, hijau, CDH CHG
+       0.5, -0.5,  0.5,     0.0, 0.0,  1.0, 0.0, 0.0,
+       0.5, -0.5, -0.5,     0.2, 0.0,  1.0, 0.0, 0.0,
+       0.5,  0.5,  0.5,     0.0, 1.0,  1.0, 0.0, 0.0,
+       0.5, -0.5, -0.5,     0.2, 0.0,  1.0, 0.0, 0.0,
+       0.5,  0.5, -0.5,     0.2, 1.0,  1.0, 0.0, 0.0,
 
-      -0.5, -0.5,  -0.5,   1.0, 1.0, 0.0, //EFG EGH KUNING BELAKANG
-      -0.5,  0.5,  -0.5,   1.0, 1.0, 0.0,
-       0.5,  0.5,  -0.5,   1.0, 1.0, 0.0,
-      -0.5,  0.5,  -0.5,   1.0, 1.0, 0.0,
-       0.5,  0.5,  -0.5,   1.0, 1.0, 0.0,
-       0.5, -0.5,  -0.5,   1.0, 1.0, 0.0,
-      -0.5, -0.5,  -0.5,   1.0, 1.0, 0.0,
-       0.5, -0.5,  -0.5,   1.0, 1.0, 0.0,
+       0.5, -0.5,  0.5,     0.2, 1.0,  0.0, -1.0, 0.0, // bawah, biru, DAE DEH
+      -0.5, -0.5,  0.5,     0.2, 0.0,  0.0, -1.0, 0.0,
+      -0.5, -0.5, -0.5,     0.4, 0.0,  0.0, -1.0, 0.0,
+       0.5, -0.5,  0.5,     0.2, 1.0,  0.0, -1.0, 0.0,
+      -0.5, -0.5, -0.5,     0.4, 0.0,  0.0, -1.0, 0.0,
+       0.5, -0.5, -0.5,     0.4, 1.0,  0.0, -1.0, 0.0,
 
-      -0.5,  0.5,  -0.5,   0.0, 1.0, 1.0, //FEA FAB CYAN KIRI
-      -0.5, -0.5,  -0.5,   0.0, 1.0, 1.0,
-      -0.5, -0.5,   0.5,   0.0, 1.0, 1.0,
-      -0.5, -0.5,  -0.5,   0.0, 1.0, 1.0,
-      -0.5, -0.5,   0.5,   0.0, 1.0, 1.0,
-      -0.5,  0.5,   0.5,   0.0, 1.0, 1.0,
-      -0.5,  0.5,  -0.5,   0.0, 1.0, 1.0,
-      -0.5,  0.5,   0.5,   0.0, 1.0, 1.0,
+      -0.5, -0.5, -0.5,     0.4, 1.0,  0.0, 0.0, -1.0, // belakang, kuning, EFG EGH
+      -0.5,  0.5, -0.5,     0.4, 0.0,  0.0, 0.0, -1.0,
+       0.5,  0.5, -0.5,     0.6, 0.0,  0.0, 0.0, -1.0,
+      -0.5, -0.5, -0.5,     0.4, 1.0,  0.0, 0.0, -1.0,
+       0.5,  0.5, -0.5,     0.6, 0.0,  0.0, 0.0, -1.0,
+       0.5, -0.5, -0.5,     0.6, 1.0,  0.0, 0.0, -1.0,
 
-       0.5,  0.5,  -0.5,   1.0, 0.0, 1.0, //GFB GBC MAGENTA ATAS
-      -0.5,  0.5,  -0.5,   1.0, 0.0, 1.0,
-      -0.5,  0.5,   0.5,   1.0, 0.0, 1.0,
-      -0.5,  0.5,  -0.5,   1.0, 0.0, 1.0,
-      -0.5,  0.5,   0.5,   1.0, 0.0, 1.0,
-       0.5,  0.5,   0.5,   1.0, 0.0, 1.0,
-       0.5,  0.5,  -0.5,   1.0, 0.0, 1.0,
-       0.5,  0.5,   0.5,   1.0, 0.0, 1.0,
+      -0.5,  0.5, -0.5,     0.6, 1.0,  -1.0, 0.0, 0.0, // kiri, cyan, FEA FAB
+      -0.5, -0.5, -0.5,     0.6, 0.0,  -1.0, 0.0, 0.0,
+      -0.5, -0.5,  0.5,     0.8, 0.0,  -1.0, 0.0, 0.0,
+      -0.5,  0.5, -0.5,     0.6, 1.0,  -1.0, 0.0, 0.0,
+      -0.5, -0.5,  0.5,     0.8, 0.0,  -1.0, 0.0, 0.0,
+      -0.5,  0.5,  0.5,     0.8, 1.0,  -1.0, 0.0, 0.0,
 
+       0.5,  0.5, -0.5,     0.8, 1.0,  0.0, 1.0, 0.0, // atas, magenta, GFB GBC
+      -0.5,  0.5, -0.5,     0.8, 0.0,  0.0, 1.0, 0.0,
+      -0.5,  0.5,  0.5,     1.0, 0.0,  0.0, 1.0, 0.0,
+       0.5,  0.5, -0.5,     0.8, 1.0,  0.0, 1.0, 0.0,
+      -0.5,  0.5,  0.5,     1.0, 0.0,  0.0, 1.0, 0.0,
+       0.5,  0.5,  0.5,     1.0, 1.0,  0.0, 1.0, 0.0
     ];
 
-    
     var triangleVertices = new Float32Array([
       +0.2, -0.5,   1.0,1.0,1.0, //1 balok kiri  
       +0.2, +0.5,   1.0,1.0,1.0, //2
@@ -132,99 +110,132 @@
       +0.5, +0.5,   1.0,1.0,1.0,//3
       +0.5, -0.5,   1.0,1.0,1.0,//4
       +0.4, -0.5,   1.0,1.0,1.0,//1 
-    ]);
-
-    function render() {
-
-      // Bersihkan buffernya canvas
-      gl.clear(gl.COLOR_BUFFER_BIT);
+     ]);
     
-      drawA(gl.TRIANGLES, triangleVertices, 1, shaders[1])
-      drawA(gl.LINES, cubeVertices, 0, shaders[0])
+    function render() {
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
       
-      // gl.drawArrays(gl.LINES, 0, 48);
-      requestAnimationFrame(render); 
+      drawA(gl.TRIANGLES, triangleVertices, 1, shaders[1]);
+      drawA(gl.TRIANGLES, cubeVertices, 0, shaders[0]);
+    
+      requestAnimationFrame(render);
     }
-    // Bersihkan layar jadi hitam
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.enable(gl.DEPTH_TEST);
+    // gl.frontFace(gl.CW);
     render();
-  }
 
-  function drawA(type, vertices, mode, program) {
-    var n = initBuffers(mode, vertices, program);
-    if (n < 0) {
-      console.log('Failed to set the positions of the vertices');
-      return;
-    }
-    gl.drawArrays(type, 0, n);
-  }
+    function drawA(type, vertices, mode, program) {
 
-  function initBuffers(mode, vertices, program) {
-    var n;
-    if (mode) {
-      n = vertices.length/5;
-    } else {
-      n = vertices.length/6;
-    }
-    // console.log(n)
-
-    var vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-      console.log('Failed to create the buffer object');
-      return -1;
+      var n = initBuffers(mode, vertices, program);
+      if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+      }
+      gl.drawArrays(type, 0, n);
     }
 
-    gl.useProgram(program);
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-    // Definisi view dan projection
-    var vmLoc = gl.getUniformLocation(program, 'view');
-    var pmLoc = gl.getUniformLocation(program, 'projection');
-    var vm = glMatrix.mat4.create();
-    var pm = glMatrix.mat4.create();
-    // console.log(pm);
-
-    glMatrix.mat4.lookAt(vm,
-      glMatrix.vec3.fromValues(0.0, 0.0, -0.5),    // posisi kamera
-      glMatrix.vec3.fromValues(0.0, 0.0, -2.0),  // titik yang dilihat; pusat kubus akan kita pindah ke z=-2
-      glMatrix.vec3.fromValues(0.0, 1.0, 0.0)   // arah atas dari kamera
-    );
-
-    var fovy = glMatrix.glMatrix.toRadian(90.0);
-    var aspect = canvas.width / canvas.height;
-    var near = 0.5;
-    var far = 10.0;
-    glMatrix.mat4.perspective(pm,
-      fovy,
-      aspect,
-      near,
-      far
-    );
-
-    gl.uniformMatrix4fv(vmLoc, false, vm);
-    gl.uniformMatrix4fv(pmLoc, false, pm);
-
-    var vPosition = gl.getAttribLocation(program, 'vPosition');
-    var vColor = gl.getAttribLocation(program, 'vColor');
-
-    function onKeyPress(event) {
-      if (event.keyCode == 88 || event.keyCode == 120) {
-        axis = xAxis;
-      } else if (event.keyCode == 89 || event.keyCode == 121) {
-        axis = yAxis;
-      } else if (event.keyCode == 90 || event.keyCode == 122) {
-        axis = zAxis;
+    
+    var lastX, lastY, dragging;
+    function onMouseDown(event) {
+      var x = event.clientX;
+      var y = event.clientY;
+      var rect = event.target.getBoundingClientRect();
+      if (rect.left <= x &&
+          rect.right > x &&
+          rect.top <= y &&
+          rect.bottom > y) {
+            lastX = x;
+            lastY = y;
+            dragging = true;
       }
     }
-    document.addEventListener('keypress', onKeyPress);
+    function onMouseUp(event) {
+      dragging = false;
+    }
+    function onMouseMove(event) {
+      var x = event.clientX;
+      var y = event.clientY;
+      if (dragging) {
+        var factor = 10 / canvas.height;
+        var dx = factor * (x - lastX);
+        var dy = factor * (y - lastY);
+        theta[yAxis] += dx;
+        theta[xAxis] += dy;
+      }
+      lastX = x;
+      lastY = y;
+    }
+    document.addEventListener('mousedown', onMouseDown);
+    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('mousemove', onMouseMove);
 
-    theta[axis] += rotAdder;  // dalam derajat
+    function initBuffers(mode, vertices, program) {
+      var n;
+      if (mode) {
+        n = vertices.length;
+      } else {
+        n = vertices.length;
+      }
+      // console.log(vertices);
 
-    if (mode) {
-        var scaleLocation = gl.getUniformLocation(program, 'scale');
-        gl.uniform1f(scaleLocation, scale);
+      var vertexBuffer = gl.createBuffer();
+      if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+      }
+
+      gl.useProgram(program);
+      gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+      // Definisi view dan projection
+      var vmLoc = gl.getUniformLocation(program, 'view');
+      var pmLoc = gl.getUniformLocation(program, 'projection');
+      var mmLoc = gl.getUniformLocation(program, 'model');
+      var vm = glMatrix.mat4.create();
+      var pm = glMatrix.mat4.create();
+      // console.log(pm);
+
+      glMatrix.mat4.lookAt(vm,
+        glMatrix.vec3.fromValues(0.0, 0.0, 0.0),    // posisi kamera
+        glMatrix.vec3.fromValues(0.0, 0.0, 0.0),  // titik yang dilihat; pusat kubus akan kita pindah ke z=-2
+        glMatrix.vec3.fromValues(0.0, 0.0, 0.0)   // arah atas dari kamera
+      );
+
+      var fovy = glMatrix.glMatrix.toRadian(90.0);
+      var aspect = canvas.width / canvas.height;
+      var near = 0.5;
+      var far = 10.0;
+      glMatrix.mat4.perspective(pm,
+        fovy,
+        aspect,
+        near,
+        far
+      );
+
+      gl.uniformMatrix4fv(vmLoc, false, vm);
+      gl.uniformMatrix4fv(pmLoc, false, pm);
+
+      var vPosition = gl.getAttribLocation(program, 'vPosition');
+      var vNormal = gl.getAttribLocation(program, 'vNormal');
+      
+      // Uniform untuk definisi cahaya
+      var lightColorLoc = gl.getUniformLocation(program, 'lightColor');
+      var lightPositionLoc = gl.getUniformLocation(program, 'lightPosition');
+      var ambientColorLoc = gl.getUniformLocation(program, 'ambientColor');
+      var lightColor = [1, 1, 1];
+      var lightPosition = [translate[0], translate[1], -2 + translate[2]];
+      var ambientColor = glMatrix.vec3.fromValues(0.17, 0.40, 0.26);
+      gl.uniform3fv(lightColorLoc, lightColor);
+      gl.uniform3fv(lightPositionLoc, lightPosition);
+      gl.uniform3fv(ambientColorLoc, ambientColor);
+
+      if (mode) {
+        
+        var vColor = gl.getAttribLocation(program, 'vColor');
+
         if (scale > 1){
           adder = -0.0026
         }
@@ -254,58 +265,118 @@
         );
 
         //Translasi X
-        if (translate[0] + 0.5 > 0.5*5 || translate[0] + 0.2 < -0.5*5 ) {
+        if (translate[0] + 0.15 > 0.5 || translate[0] - 0.15 < -0.5 ) {
           xAdders *= -1;
         }
         translate[0] += xAdders;
+        // console.log(translate[0] + xAdders);
 
-        var middleLoc = gl.getUniformLocation(program, 'middle_coorinates');
-        middle_coordinates = 0.35 + translate[0];
-
-        gl.uniform1f(middleLoc, middle_coordinates);
-        // console.log(translate[0])
+        current_middle += translate[0];
 
         //Translasi Y
-        if (translate[1] + 0.5 > 0.5*5 || translate[1] + -0.5 < -0.5*5 ) {
+        if (translate[1] + 0.2 > 0.5 || translate[1] - 0.2 < -0.5 ) {
           yAdders *= -1;
         }
         translate[1] += yAdders;
 
         //Translasi Z
-        if (translate[2] > 0.5*0.5 || translate[2] < -0.5*0.5 ) {
+        if (translate[2] > 0.5 || translate[2] < -0.5 ) {
           zAdders *= -1;
         }
         translate[2] += zAdders;
 
-        var translationLoc = gl.getUniformLocation(program, 'translate');
-
-        gl.uniform3fv(translationLoc, translate);
-
-        var thetaLoc = gl.getUniformLocation(program, 'theta');
-
-        gl.uniform3fv(thetaLoc, theta);
-    } else{
+        var mm = glMatrix.mat4.create();
+        glMatrix.mat4.translate(mm, mm, [0.0, 0.0, -2.0]);
+        // console.log(translate);
+        glMatrix.mat4.translate(mm, mm, translate);
+        glMatrix.mat4.scale(mm, mm, [0.2, 0.2, 0.2]);
+        glMatrix.mat4.scale(mm, mm, [scale, 1.0, 1.0]);
+        // glMatrix.mat4.rotateZ(mm, mm, theta[zAxis]);
+        // glMatrix.mat4.rotateY(mm, mm, theta[yAxis]);
+        // glMatrix.mat4.rotateX(mm, mm, theta[xAxis]);
+        gl.uniformMatrix4fv(mmLoc, false, mm);
+        
+        gl.enableVertexAttribArray(vColor);
+      } else{
+  
+        var vTexCoord = gl.getAttribLocation(program, 'vTexCoord');
         gl.vertexAttribPointer(
           vPosition,  // variabel yang memegang posisi attribute di shader
           3,          // jumlah elemen per attribute
           gl.FLOAT,   // tipe data atribut
           gl.FALSE,
-          6 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap verteks 
+          8 * Float32Array.BYTES_PER_ELEMENT, // ukuran byte tiap verteks 
           0                                   // offset dari posisi elemen di array
         );
+        gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, gl.FALSE, 
+          8 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+        gl.enableVertexAttribArray(vTexCoord);
+
+        // theta[axis] += glMatrix.glMatrix.toRadian(0.5);  // dalam derajat
+        var mm = glMatrix.mat4.create();
+        glMatrix.mat4.translate(mm, mm, [0.0, 0.0, -2.0]);
+        // glMatrix.mat4.rotateZ(mm, mm, theta[zAxis]);
+        // glMatrix.mat4.rotateY(mm, mm, theta[yAxis]);
+        // glMatrix.mat4.rotateX(mm, mm, theta[xAxis]);
+        gl.uniformMatrix4fv(mmLoc, false, mm);
         
-        gl.vertexAttribPointer(vColor, 3, gl.FLOAT, gl.FALSE, 
-          6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+        // Uniform untuk tekstur
+        var sampler0Loc = gl.getUniformLocation(program, 'sampler0');
+        gl.uniform1i(sampler0Loc, 0);
+        // Inisialisasi tekstur
+        var texture = gl.createTexture();
+        if (!texture) {
+          reject(new Error('Gagal membuat objek tekstur'));
+        }
+        gl.activeTexture(gl.TEXTURE0);
+        initTexture(texture);
+      }
+      
+      gl.vertexAttribPointer(vNormal, 3, gl.FLOAT, gl.FALSE, 
+        8 * Float32Array.BYTES_PER_ELEMENT, 5 * Float32Array.BYTES_PER_ELEMENT);
 
-        var thetaLoc = gl.getUniformLocation(program, 'theta');
+      var nmLoc = gl.getUniformLocation(program, 'normalMatrix');
 
-        gl.uniform3fv(thetaLoc, theta);
+      // Perhitungan modelMatrix untuk vektor normal
+      var nm = glMatrix.mat3.create();
+      glMatrix.mat3.normalFromMat4(nm, mm);
+      gl.uniformMatrix3fv(nmLoc, false, nm);
 
+      //Rotation Matrix
+      var mvpLoc = gl.getUniformLocation(program, 'MVPMatrix');
+      var mvp = glMatrix.mat4.create();
+      glMatrix.mat4.multiply(mvp,vm,mm);
+      glMatrix.mat4.multiply(mvp,pm,mvp);
+      glMatrix.mat4.rotateY(mvp, mvp, theta[yAxis]);
+      glMatrix.mat4.rotateX(mvp, mvp, theta[xAxis]);
+
+      gl.uniformMatrix4fv(mvpLoc, false, mvp);
+
+      gl.enableVertexAttribArray(vPosition);
+      gl.enableVertexAttribArray(vNormal);
+      return n;
     }
 
-    gl.enableVertexAttribArray(vPosition);
-    gl.enableVertexAttribArray(vColor);
-    return n;
+    // Membuat mekanisme pembacaan gambar jadi tekstur
+    function initTexture(texture) {
+      var imageSource = 'images/selfie.jpg';
+      var image = new Image();
+      if (!image) {
+        reject(new Error('Gagal membuat objek gambar'));
+      }
+      image.onload = function() {
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+      }
+      image.src = imageSource;
+    }
   }
 
 })();
+
+
+
